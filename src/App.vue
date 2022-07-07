c66a9d81f94bd5ee097d10fc03f7cfa0aacef523
Laurence Cadag <laurencecads1@gmail.com>
2022-07-07 15:07:02 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar/>
+  <HeroSection/>
+  <ProjectSection/>
+  <TechStack/>
+  <Footer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Navbar from './components/Navbar.vue'
+import HeroSection from "./components/Hero.vue"
+import ProjectSection from "./components/ProjectSection.vue"
+import TechStack from './components/TechStack.vue'
+import Footer from "./components/Footer.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    HeroSection,
+    ProjectSection,
+    TechStack,
+    Footer
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap');
+html {
+  scroll-behavior: smooth;
+}
+* {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  text-decoration: none;
+  font-family: 'Lato', sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.hero {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.65),rgba(0, 0, 0, 0.8)),url("./assets/background.jpg");
+  height: 80vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.heroText {
+  padding: 0 2rem;
+}
+
+.owner, .ownerTitle, .ownerDescription {
+  margin-bottom: 1.2rem;
+}
+
+.owner {
+  font-size: 4.5rem;
+  font-weight: 700;
+}
+
+.ownerTitle {
+  font-size: 3rem;
+  font-weight: 300;
+}
+
+.ownerDescription {
+  font-size: 2rem;
+  font-weight: 200;
+  text-shadow: 2px 1px 0px rgba(0, 0, 0, 1);
+}
+
+.know {
+  padding: .8rem;
+  font-weight: 900;
+  text-transform: uppercase;
+  background-color: #e28743;
+  color: white;
+  border-radius: 4px;
+  border: none;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add( faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter)
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
